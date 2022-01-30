330ed2559299d887899294852e6aca9a5f450119
smilesandsobs <nmlamperti@gmail.com>
2022-01-29 20:28:32 -0800

    13.5.5

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -5,7 +5,7 @@ console.log("working");
 //let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY,
@@ -20,17 +20,17 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 
 // Create a base layer that holds both maps.
-let baseMaps = {
-    Street: streets,
+let baseMaps = {   
+    light: light,
     Dark: dark
   };
 
 
   // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [30, 30],
+    center: [44.0, -80.0],
     zoom: 2,
-    layers: [streets]
+    layers: [light]
 })
 
 
@@ -38,14 +38,13 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/smilesandsobs/Mapping_Earthquakes/main/majorAirports.json";
-
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/smilesandsobs/Mapping_Earthquakes/main/torontoRoutes.json";
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
-    console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+d3.json(torontoData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data).addTo(map);
 });
 
